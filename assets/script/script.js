57f4dcc4e755d2c63e2a910b3bbe710537e38023
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-01-27 14:06:57 +0000

    added a function that displays current weather for any city the user inputs

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,11 @@
 // when the document is ready, run this function 
 $(document).ready(function () {
     // event listener for the search button
-    var lat = "";
-    var lon = "";
-
     $("#search-button").on("click", function (event) {
         // prevent the default action of the event
         event.preventDefault();
         // get the value of the search input
-        var city = $("#search-input").val();
-
+        var city = $("#search-input").val().trim();
         // if the city is not empty fetch the lon and lat of the city
         if (city) {
             fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city
@@ -23,7 +19,6 @@ $(document).ready(function () {
                     // get the lon and lat from the data returned from the 1st fetch 
                     var lat = data[0].lat.toString();
                     var lon = data[0].lon.toString();
-
                     // fetch the weather data using the lon and lat
                     fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat
                         + "&lon=" + lon + "&units=metric&appid=7edf678be3dfbb52ca6176d7c38aa0f0")
@@ -33,7 +28,8 @@ $(document).ready(function () {
                         .then(function (data) {
                             // display the data from the second fetch 
                             console.log(data);
-                            // displayWeatherData(data);
+                            displayCurrentWeather(data);
+                            // displayForecast(data);
                         });
                 });
         }
@@ -42,6 +38,23 @@ $(document).ready(function () {
         }
     });
 
+    // function to display the weather data
+    function displayCurrentWeather(data){
+        // get the forecast data from the data returned from the fetch 
+        var forecast = data.list;
+        // get the current date
+        var today = dayjs().format('(DD/MM/YYYY)');
+        // todays forecast 
+        $("#today").empty();
+        var todayHeadingEl = $("<h2>").text(data.city.name+ " " + today);
+        var todayIconEl = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast[0].weather[0].icon + ".png");
+        var todaytempEl = $("<p>").text("Temperature: " + forecast[0].main.temp + "°C");
+        var todaywindEl = $("<p>").text("Wind: " + forecast[0].wind.speed + " MPH");
+        var todayhumidityEl = $("<p>").text("Humidity: " + forecast[0].main.humidity + "%");
+
+        todayHeadingEl.append(todayIconEl);
+        $("#today").append(todayHeadingEl, todaytempEl, todaywindEl, todayhumidityEl);
+    }
    
 
 });
