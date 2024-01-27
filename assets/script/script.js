de63311bf1b1ed2926843ebed5f7cf0681554450
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-01-27 14:52:50 +0000

    added an event listener to the history buttons so that the user can click on the button and get weather data

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
         event.preventDefault();
         // get the value of the search input
         var city = $("#search-input").val().trim();
+        //clear the search input
+        $("#search-input").val("");
         // store the city in local storage
         storeCity(city);
         // if the city is not empty fetch the lon and lat of the city
@@ -42,6 +44,39 @@ $(document).ready(function () {
         else {
             alert("Please enter a city");
         }
+    });
+    
+    // event listener for the history buttons
+    $("#history").on("click", "#history-button", function (event) {
+        // prevent the default action of the event
+        event.preventDefault();
+        // get the city name from the button
+        var city = $(this).attr("data-city");
+        // fetch the lon and lat of the city
+        fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city
+            + "&limit=1&appid=7edf678be3dfbb52ca6176d7c38aa0f0")
+            .then(function (response) {
+                return response.json();
+            })
+            // when the data is converted to JSON, call the function to get the weather data
+            .then(function (data) {
+                console.log(data);
+                // get the lon and lat from the data returned from the 1st fetch 
+                var lat = data[0].lat.toString();
+                var lon = data[0].lon.toString();
+                // fetch the weather data using the lon and lat
+                fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat
+                    + "&lon=" + lon + "&units=metric&appid=7edf678be3dfbb52ca6176d7c38aa0f0")
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        // display the data from the second fetch 
+                        console.log(data);
+                        displayCurrentWeather(data);
+                        displayForecast(data);
+                    });
+            });
     });
 
     // function to display the weather data
